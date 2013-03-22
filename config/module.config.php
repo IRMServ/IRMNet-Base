a14f3e3122f6900c7e89641b74ec3f5d6a89155f
IgorDePaula <principe.borodin@gmail.com>
2013-03-22 09:07:32 -0300

    Reparo na navegacao do modulo

--- config/module.config.php
+++ config/module.config.php
@@ -62,4 +62,14 @@ return array(
             __DIR__ . '/../view',
         ),
     ),
+        'navigation' => array(
+        // The DefaultNavigationFactory we configured in (1) uses 'default' as the sitemap key
+        'default' => array(
+            // And finally, here is where we define our page hierarchy
+            'base' => array(
+                'label' => 'Página principal',
+                'route' => 'home',
+            ),
+        ),
+    ),
 );
